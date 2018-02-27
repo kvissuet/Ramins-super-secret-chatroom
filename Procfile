37e693bb94219d84e1775f991f24dee05e8da881
kvissuet <kvissuet@gmail.com>
2018-02-26 20:59:45 -0600

    Ready

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Secrets.wsgi --log-file -
---
+++ intro/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from intro.models import Topic, Entry
+# Register your models here.
+
+
+admin.site.register(Entry)
+admin.site.register(Topic)
