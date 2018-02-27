8f70b404b58effe370f0083b5a0b6740037ce4f2
kvissuet <kvissuet@gmail.com>
2018-02-26 21:24:06 -0600

    Ready

--- secret/settings.py
+++ secret/settings.py
@@ -128,6 +128,16 @@ STATIC_URL = '/static/'
 LOGIN_URL = '/users/login/'
 
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # Heroku settings
 if os.getcwd() == '/app':
 	import dj_database_url
